a783625fb117ae00dfbb88ae88bc0182a737defc
MsPtibiscuit <frederic.kaczynski@gmail.com>
2014-08-30 22:35:44 +0200

    Fix datasource_list.blade template

--- src/app/views/datasource_list.blade.php
+++ src/app/views/datasource_list.blade.php
@@ -2,10 +2,14 @@
 
 @section("content")
 	Listage des sources de données:<br />
-	@foreach($datasources as $datasource)
-		<div>
-			{{{ $datasource.name }}}
-		</div>
-	@stop
+	@if (count($datasources) > 0)
+		@foreach ($datasources as $datasource)
+			<div>
+				{{{ $datasource.name }}}
+			</div>
+		@endforeach
+	@else
+		Aucune source de données
+	@endif
 
 @stop
